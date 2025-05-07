25fee253ecbaa65aeb5af3d6843e6edb29dd1507
MatheusNespolo <nespolo.m1@gmail.com>
2025-05-07 13:37:33 -0300

    começando o cronômetro
    vo ter que dar um jeito de testar. Commit e era isso!

--- src/script.js
+++ src/script.js
@@ -1,1 +1,35 @@
 // 
+const botaoInicio = document.getElementById("botaoInicio");
+const Tempo1 = document.getElementById("Tempo1");
+const Tempo2 = document.getElementById("Tempo2");
+const Tempo3 = document.getElementById("Tempo3");
+const Tempo4 = document.getElementById("Tempo4");
+const Atividade1 = document.getElementById("Atividade1");
+const Atividade2 = document.getElementById("Atividade2");
+const Atividade3 = document.getElementById("Atividade3");
+const Atividade4 = document.getElementById("Atividade4");
+
+// 
+botaoInicio.addEventListener("click", () => {
+    const tempos = [Tempo1, Tempo2, Tempo3, Tempo4];
+    for (let i = 0; i < tempos.length; i++) {
+        cronometro(tempos[i].value);
+    }
+});
+
+// 
+function cronometro(tempo) {
+    let minutos = tempo;
+    let segundos = 0;
+    let intervalo;
+
+    intervalo = setInterval(() => {
+        segundos--;
+        if (segundos < 0) {
+            segundos = 59;
+            minutos--;
+        }
+        if (minutos < 0) {
+            clearInterval(intervalo);
+        }
+    }, 1000);}
